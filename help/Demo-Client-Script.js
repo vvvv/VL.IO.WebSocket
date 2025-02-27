d1d42f2918e6d9727970c8b9821f4f53896100fe
antongit <anton@vvvv.org>
2025-02-27 14:04:52 +0100

    Added Server/Client Example

---
+++ help/Demo-Client-Script.js
@@ -0,0 +1,71 @@
+window.addEventListener("load", () => {
+
+    const form = document.getElementById("Form")
+    const x = document.getElementById("x")
+    const y = document.getElementById("y")
+
+    var socket = null
+
+    function sendData()
+    {
+        const message = serialize(form)
+
+        try {
+            socket.send(message);
+        }
+        catch (error){
+            console.log (error)
+        }
+    }
+
+
+    try{
+        socket = new WebSocket('ws://127.0.0.1:4444');
+        socket.onopen = function(event) {
+            console.log('WebSocket connectioned');
+            sendData()
+        }
+        socket.onerror = function(error) {
+            console.error('WebSocket error: ' + error);
+        }
+        socket.onmessage = function(message){
+            const data = JSON.parse(message.data)
+            x.value = data[0]
+            y.value = data[1]
+        }
+        socket.onclose = function()
+        {
+            socket.send("Closing");
+        }
+    }
+    catch{
+        console.log ("Can't connect")
+    }
+
+    form.addEventListener("input", sendData);
+});
+
+function serialize(obj)
+{   
+    var data = {}
+    var formData = new FormData(obj)
+    for (var key of formData.keys()) {
+        var value = formData.get(key)
+        value = key == "Size" ? parseFloat(value) : value
+        value = key == "Color" ? HexToArray(value) : value 
+        data[key] = value
+    }
+    return JSON.stringify(data)
+}
+
+function HexToArray (hex)
+{
+    const tempHex = hex.replace('#', '')
+    const color=[
+        parseInt(tempHex.substring(0, 2), 16)/255,
+        parseInt(tempHex.substring(2, 4), 16)/255,
+        parseInt(tempHex.substring(4, 6), 16)/255,
+        1
+    ]
+    return color
+}
